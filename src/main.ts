84ee4f51a0d768cb6577edb137032b5cc73b116c
Shubham Gupta <shubhamedu.01@gmail.com>
2025-03-13 17:11:40 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import './assets/main.css'
 
-createApp(App).use(store).mount("#app");
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
